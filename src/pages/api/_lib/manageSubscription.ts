280e347192d38f06155be3cd9405df27cf782499
Diego Masin <diegoifce@gmail.com>
2021-12-18 12:31:39 -0300

    Ajust error.

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,38 +1,33 @@
-import {query as q} from 'faunadb'
+import { query as q } from "faunadb";
 
 import { fauna } from "../../../services/fauna";
-import { stripe } from '../../../services/stripe';
+import { stripe } from "../../../services/stripe";
 
-export async function saveSubscription(subscriptionId: string, customerId: string) {
-     // Search user on Faunadb
-     const userRef = await fauna.query(
-         q.Select(
-             "ref",
-             q.Get(
-                q.Match(
-                    q.Index('user_by_stripe_customer_id'),
-                    customerId
-                )
-            )
-         )
-     )
-     console.log('Passei aqui 2')
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string
+) {
+  // Search user on Faunadb
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
+    )
+  );
+  console.log("Passei aqui 2");
 
-     const subscription = await stripe.subscriptions.retrieve(subscriptionId)
-     
-     // Save subscription's data on FaunaDb
-     const subscriptionData = {
-         id: subscription.id,
-         userId: userRef,
-         status: subscription.status,
-         price_id: subscription.items.data[0].price.id
-     }
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
 
-     console.log('Passei aqui 3')
-     await fauna.query(
-         q.Create(
-             q.Collection('subscriptions'),
-             {data: subscriptionData}
-         )
-     )
+  // Save subscription's data on FaunaDb
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  };
+
+  console.log("Passei aqui 3");
+  await fauna.query(
+    q.Create(q.Collection("subscriptions"), { data: subscriptionData })
+  );
 }
