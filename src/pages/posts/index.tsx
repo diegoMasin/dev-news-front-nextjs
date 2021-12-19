1e5463b83217a80d0289fb7697af5909f815fe97
Diego Masin <diegoifce@gmail.com>
2021-12-19 12:01:58 -0300

    Formating data. Listing posts.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,10 +2,21 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+interface Post {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,33 +24,13 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>19 de Dezembro de 2021</time>
-            <strong>A guide to Flexbox properties in React Native</strong>
-            <p>
-              If you have a web design and/or development background, you may be
-              familiar with Flexbox. It is an opt-in CSS tool that enables you
-              to build layouts based on columns and rows.
-            </p>
-          </a>
-          <a href="#">
-            <time>19 de Dezembro de 2021</time>
-            <strong>A guide to Flexbox properties in React Native</strong>
-            <p>
-              If you have a web design and/or development background, you may be
-              familiar with Flexbox. It is an opt-in CSS tool that enables you
-              to build layouts based on columns and rows.
-            </p>
-          </a>
-          <a href="#">
-            <time>19 de Dezembro de 2021</time>
-            <strong>A guide to Flexbox properties in React Native</strong>
-            <p>
-              If you have a web design and/or development background, you may be
-              familiar with Flexbox. It is an opt-in CSS tool that enables you
-              to build layouts based on columns and rows.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -51,7 +42,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
   const response = await prismic.query(
-    // Same type saved in Prismic.io
+    // Same type saved in Prismic.io ("post")
     [Prismic.predicates.at("document.type", "post")],
     {
       fetch: ["post.title", "post.content"],
@@ -59,9 +50,25 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(response);
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title), // don't use -> post.data.title[0].text
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: { posts },
   };
 };
